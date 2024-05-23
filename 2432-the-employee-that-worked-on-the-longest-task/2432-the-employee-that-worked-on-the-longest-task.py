7646b023a06ed53be6b7bb7d41393bbffc69d845
Herbert Sitsofe Degadzor <128437531+Flyview16@users.noreply.github.com>
2024-05-23 08:35:51 +0000

    Time: 247 ms (89.19%), Space: 16.7 MB (59.07%) - LeetHub

--- 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
+++ 2432-the-employee-that-worked-on-the-longest-task/2432-the-employee-that-worked-on-the-longest-task.py
@@ -1,17 +1,19 @@
 class Solution:
     def hardestWorker(self, n: int, logs: List[List[int]]) -> int:
-        time_units = 0
-        employee_id = 0
-        for i in range(len(logs)):
-            new_time = 0
-            if i == 0:
-                new_time = logs[i][1] - 0
-            else:
-                new_time = logs[i][1] - logs[i-1][1]
-                
-            if new_time > time_units:
-                time_units = new_time
-                employee_id = logs[i][0]
-            elif new_time ==  time_units:
-                employee_id = min(employee_id, logs[i][0])
+        max_time = 0
+        employee_id = float('inf')
+        previous_time = 0
+
+        for log in logs:
+            current_id,current_time = log
+            work_time = current_time - previous_time
+
+            if work_time > max_time:
+                max_time = work_time
+                employee_id = current_id
+            elif work_time == max_time:
+                employee_id = min(employee_id, current_id)
+            
+            previous_time = current_time
+
         return employee_id
