58bffd5f1d59711db5df57a0e6365b4d1bbca364
Herbert Sitsofe Degadzor <128437531+Flyview16@users.noreply.github.com>
2024-05-24 19:24:47 +0000

    Time: 35 ms (54.5%), Space: 16.5 MB (65.94%) - LeetHub

--- 0504-base-7/0504-base-7.py
+++ 0504-base-7/0504-base-7.py
@@ -2,7 +2,11 @@ class Solution:
     def convertToBase7(self, num: int) -> str:
         output = []
         is_negative = num < 0
-        num = abs(num)
+        num = abs(num)   # work with absolut value of num
+
+        if num == 0:
+            return "0"
+
         while num > 0:
             output.append(str(num % 7))
             num //= 7
